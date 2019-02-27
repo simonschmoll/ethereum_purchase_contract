5688d9e508babc11e92586b96a9d9337d34804be
Simon Schmoll <simon.schmoll@googlemail.com>
2019-02-27 09:35:23 +0100

    cleanup code, unused commented lines

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -16,54 +16,5 @@ export default new Router({
         deploy: Deploy,
       },
     },
-  //   {
-  //     path: '/build',
-  //     name: 'Build',
-  //     components: {
-  //       default: RobotBuilder,
-  //       sidebar: SidebarBuild,
-  //     },
-  //   },
-  //   {
-  //     path: '/parts/browse',
-  //     name: 'BrowseParts',
-  //     component: BrowseParts,
-  //     children: [
-  //       {
-  //         name: 'BrowseHeads',
-  //         path: 'heads',
-  //         component: RobotHeads,
-  //       },
-  //       {
-  //         name: 'BrowseArms',
-  //         path: 'arms',
-  //         component: RobotArms,
-  //       },
-  //       {
-  //         name: 'BrowseTorsos',
-  //         path: 'torsos',
-  //         component: RobotTorsos,
-  //       },
-  //       {
-  //         name: 'BrowseBases',
-  //         path: 'bases',
-  //         component: RobotBases,
-  //       },
-  //     ],
-  //   }, {
-  //     path: '/parts/:partType/:id',
-  //     name: 'Parts',
-  //     component: PartInfo,
-  //     props: true,
-  //     beforeEnter(to, from, next) {
-  //       const isValidId = Number.isInteger(Number(to.params.id));
-  //       next(isValidId);
-  //     },
-  //   },
-  //   {
-  //     path: '/cart',
-  //     name: 'Cart',
-  //     component: ShoppingCart,
-  //   },
   ],
 });
